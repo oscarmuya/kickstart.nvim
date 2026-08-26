9e3490857e6a456d195253f352e48210b6155277
Oscar Muya <oscarmuyalx@gmail.com>
2026-08-26 11:42:41 +0300

    feat: Added typescritp language server and keymap for code diagnostics

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -4,3 +4,6 @@
 vim.keymap.set('n', '<leader>e', '<cmd>wincmd w<cr>', { desc = 'Toggle between windows' })
 vim.keymap.set('x', '<leader>p', '"_dP', { desc = 'Paste without updating the register' })
 vim.keymap.set('n', '<leader>w', '<cmd>w<cr>', { desc = 'Save file' })
+
+-- Code diagnostics
+vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Open [C]ode [D]iagnostic message' })
